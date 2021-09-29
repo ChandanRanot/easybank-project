faa114f9b7ffd66211ef008bce5ecdd46b379092
Chandan Kumar <iamchandanrajput3@gmail.com>
2021-09-29 17:39:25 +0530

    Modify project

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 function myFunction() {
     var x = document.getElementById("links");
+    var icon = document.getElementById("dropdown-icon");
     if(x.style.display === "block"){
         x.style.display = "none";
+        x.style.boxShadow = "none";
+        icon.src = "./images/icon-hamburger.svg";
     }
     else{
         x.style.display = "block";
+        x.style.boxShadow = "-10px 10px 100px";
+        icon.src = "./images/icon-close.svg";
     }
 }
